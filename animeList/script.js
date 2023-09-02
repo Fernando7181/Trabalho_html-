6e3ec40dc7c0aec39aa03e0f79a422d9152f00be
Nowaru <61397438+Nowaru159@users.noreply.github.com>
2023-09-02 18:58:30 -0300

    animeList
    a mais atualizada , com pesquisa ;)

---
+++ animeList/script.js
@@ -0,0 +1,70 @@
+
+
+function parseCSV(csvData) {
+
+    var rows = csvData.split("\n");
+
+
+    var header = rows.shift();
+
+
+    for (var i = 0; i < rows.length; i++) {
+        var columns = rows[i].split(";");
+    
+
+
+        var animeElement = document.createElement("div");
+        animeElement.className = "anime";
+
+ 
+        var titleElement = document.createElement("h2");
+        titleElement.textContent = columns[1];
+        animeElement.appendChild(titleElement);
+
+
+        var descriptionElement = document.createElement("p");
+        descriptionElement.textContent = columns[3];
+        animeElement.appendChild(descriptionElement);
+
+
+        document.getElementById("animeList").appendChild(animeElement);
+    }
+}
+
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "anime-data.csv", true);
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        parseCSV(xhr.responseText);
+    }
+};
+xhr.send();
+
+
+function search(query) {
+
+    var animeElements = document.querySelectorAll('.anime');
+
+
+    for (var i = 0; i < animeElements.length; i++) {
+        var titleElement = animeElements[i].querySelector('h2');
+        var descriptionElement = animeElements[i].querySelector('p');
+
+
+        var title = titleElement.textContent;
+        var description = descriptionElement.textContent;
+
+        if (title.toLowerCase().includes(query.toLowerCase()) || description.toLowerCase().includes(query.toLowerCase())) {
+            animeElements[i].style.display = 'block';
+        } else {
+            animeElements[i].style.display = 'none';
+        }
+    }
+}
+
+
+document.getElementById('searchInput').addEventListener('input', function() {
+    var query = this.value;
+    search(query);
+});

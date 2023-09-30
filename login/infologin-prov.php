703a5f01042881313a0118589af3f04e8e7cced9
IsaacR021 <114304790+IsaacR021@users.noreply.github.com>
2023-09-30 12:14:07 -0300

    Revert "Tela de Login"

--- login/infologin-prov.php
+++
@@ -1,5 +0,0 @@
-'<script>
-            document.addEventListener("DOMContentLoaded", function() {
-                document.getElementById("info-mensagem").innerHTML = "<p class=\'infologin\'>Login Realizado.</p>";
-            });
-          </script>';
--- login/infologin.php
+++
@@ -1,5 +0,0 @@
-'<script>
-            document.addEventListener("DOMContentLoaded", function() {
-                document.getElementById("info-mensagem").innerHTML = "<p class=\'infologin\'>Login ou senha incorretos.</p>";
-            });
-          </script>';
